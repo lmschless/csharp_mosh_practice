381349d7bbe1bd20302f34b78000bbd05831f993
Luke <lmschless@gmail.com>
2020-04-01 21:20:30 -0700

    working on try parse and adding methods to practice file

--- Program.cs
+++ Program.cs
@@ -3,51 +3,90 @@ using Microsoft.VisualBasic.CompilerServices;
 
 namespace Classes
 {
-
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("HI {0}, I am {1}", to, Name);
-        }
-
-        // declare as static so we don't need to create a person obj
-        // first before parsing
-
-        // a static method is ONLY accessible from the CLASS and not the OBJECT.
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            // don't need to create a new Person because the Parse method is static.
-            //var person = new Person();
-            //var p = Person.Parse("John");
-            //p.Introduce("Mosh");
-
-            // old syntax
-            //var customer = new Customer(1, "John");
-            // new syntax (object initialization syntax)
-            var customer = new Customer
+            //var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
             {
-                Id = 1,
-                Name = "John"
-            };
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+        }
+        public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                var sum = 0;
+                foreach (var number in numbers)
+                {
+                    sum += number;
+                }
+                return sum;
+            }
+        }
 
-            var order = new Order();
-            customer.Orders.Add(order);
+        public class Person
+        {
+            public string Name;
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            public void Introduce(string to)
+            {
+                Console.WriteLine("HI {0}, I am {1}", to, Name);
+            }
 
+            // declare as static so we don't need to create a person obj
+            // first before parsing
+
+            // a static method is ONLY accessible from the CLASS and not the OBJECT.
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+                return person;
+            }
+        }
+
+        // don't need to create a new Person because the Parse method is static.
+        //var person = new Person();
+        //var p = Person.Parse("John");
+        //p.Introduce("Mosh");
+
+        // old syntax
+        //var customer = new Customer(1, "John");
+        // new syntax (object initialization syntax)
+        //var customer = new Customer
+        //{
+        //    Id = 1,
+        //    Name = "John"
+        //};
+
+        //var order = new Order();
+        //customer.Orders.Add(order);
+
+        //Console.WriteLine(customer.Id);
+        //Console.WriteLine(customer.Name);
+
+
+
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
             try
             {
                 var point = new Point(10, 20);
@@ -61,9 +100,7 @@ namespace Classes
             {
                 Console.WriteLine("An unexpected error occured");
             }
-
-
-
         }
+
     }
 }
